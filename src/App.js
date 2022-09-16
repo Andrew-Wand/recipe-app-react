6b91670cc130e8dbd7a7ee2539dabb474d5dceb3
Andrew Wand <andrewwands@gmail.com>
2022-09-15 20:07:41 -0500

    able to search through recipes and display them

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Recipe from "./Components/Recipe";
 const App = () => {
   
   const [recipes, setRecipes] = useState([]);
+  const[search, setSearch] = useState();
+  const [query, setQuery] = useState();
   
   const API_ID = '92e3e3fb';
   const API_KEY = '2ab633677e5c7f99c9fa698846eb1858'
@@ -11,17 +13,24 @@ const App = () => {
 
   useEffect(() => {
     getRecipe();
-  }, []);
+  }, [query]);
 
 
   const getRecipe = async () => {
-    const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${API_ID}&app_key=${API_KEY}`);
+    const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${API_ID}&app_key=${API_KEY}`);
     const data = await response.json()
     console.log(data.hits);
     setRecipes(data.hits);
+  }
 
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  }
 
-    
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search) 
+    setSearch('');
   }
 
   
@@ -33,8 +42,8 @@ const App = () => {
   return (
     <div className="App">
       <h1>Title</h1>
-      <form  className="search-form">
-        <input type="text" className="search-input" />
+      <form  className="search-form" onSubmit={getSearch}>
+        <input type="text" className="search-input" value={search} onChange={updateSearch} />
         <button className="search-btn">Search</button>
       </form>
       <div className="recipes">
