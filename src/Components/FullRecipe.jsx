f9d22d15f5f7e0bee1f5047bfe97bc9fbd89876a
Andrew Wand <andrewwands@gmail.com>
2022-09-24 19:16:03 -0500

    added recipe info on full recipe page

--- src/Components/FullRecipe.jsx
+++ src/Components/FullRecipe.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Link, useParams, useLocation } from "react-router-dom";
 
-function FullRecipe() {
+function FullRecipe({ingredients}) {
+
 const params = useParams();
 const location = useLocation();
 const data = location.state?.data;
@@ -12,8 +13,19 @@ const data = location.state?.data;
         {data && (
             <div>
                 <h3>{params.name}</h3>
+                <img src={data.recipe.image} alt="img" />
+                <div className="ingredients-container">
+                    <ol>
+                        {data.recipe.ingredients.map(ingredient => (
+                            <li>{ingredient.text}</li>
+                        ))}
+                    </ol>
+                </div>
             </div>
         )}
+
+
+        <Link to='/'>Home</Link>
     </div>
   )
 }
